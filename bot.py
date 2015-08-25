b95ba05bc4faf024c23015e7915932e56a2f38aa
Eitan Romanoff <ear7631@gmail.com>
2015-08-24 21:40:49 -0400

    Added a boats function. I'm sorry Kigen...

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
-import discord, sys
+import discord, sys, os, json
 
 
 client = discord.Client()
+HELP_MSG = """\
+Ti Discord Bot Functions:
+!help               - displays this information
+!boat <thing>++/--   - upboat or downboat a thing (useless)
+""" 
 
 
 def main():
-    global client
     if len(sys.argv) != 3:
         print "Usage: python bot.py <email> <password>"
     client.login(sys.argv[1], sys.argv[2])
@@ -16,6 +20,10 @@ def main():
 def on_message(message):
     if message.content.startswith('!test'):
         client.send_message(message.channel, 'Ti Discord Bot is up and running!')
+    elif message.content.startswith("!boat"):
+        boat(message)
+    elif message.content.startswith("!help"):
+        client.send_message(message.channel, HELP_MSG)
 
 
 @client.event
@@ -24,6 +32,72 @@ def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+
+
+def boat(message):
+    # create the file if it doesn't exist
+    if not os.path.isfile("boats.dat"):
+        f = open("boats.dat", "w")
+        f.write("{}")
+        f.close()
+
+    # load the data as json
+    f = open("boats.dat", "r")
+    data = json.loads(f.read())
+    f.close()
+
+    thing = message[len("!boat "):].strip()
+    # use lower for key access
+    kthing = thing.lower()
+
+    # !boat - list all boats
+    if not thing:
+        s = "All of these things have boats:"
+        for k in data:
+            s = s + k + ": " + data[k] + "\n"
+        client.send_message(message.channel, s)
+        return
+    
+    op = thing[-2:]
+
+    # !karma ++  not allowed!
+    if op == thing:
+        return
+
+    # !boat <thing> - list the boat of the thing
+    if op not in ("++", "--") and kthing in data:
+        client.send_message(message.channel, thing + " has " + data[kthing] + " boats.")
+        return
+
+    thing = thing[:-2]
+    if op == "++":
+        if kthing not in data:
+            data[kthing] = 1
+        else:
+            data[kthing] = data[kthing] + 1
+    elif op == "--":
+        if kthing not in data:
+            data[kthing] = -1
+        else:
+            data[kthing] = data[kthing] - 1
+
+    client.send_message(message.channel, "Upboats for " + thing + "! " + thing[0].upper() + thing[1:] + " now has " + data[kthing] + " boats.")
+    f = open("boats.dat", "w")
+    f.write(json.dumps(data))
+    f.close()
+    return
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
