25ca389d3f513720c33fc7e81cd5dcabf579c36b
Allonte <angelosheltie@gmail.com>
2015-08-27 11:31:47 -0700

    Twitter polling using register event model  as hooks for events

--- ti_twitter.py
+++ ti_twitter.py
@@ -4,12 +4,20 @@ import threading
 from dateutil import parser
 from time import sleep, time
 
+#TODO: Use filelock to ensure thread safe file IO
+
+def _null_event(*args, **kwargs):
+    pass
+
 class TwitterPoll(threading.Thread):
     def __init__(self, access_token_key, access_token_secret, consumer_key, consumer_secret, polling_seconds=60):
         super(TwitterPoll, self).__init__(name="TwitterPollingThread")
         self.setDaemon(True)
 
         self.polling_seconds = polling_seconds
+
+        self.events = {"new_tweet": _null_event,
+                       "no_tweets": _null_event}
 
         self._auth = twitter.oauth.OAuth(access_token_key, access_token_secret, consumer_key, consumer_secret)
         self.twitter = None
@@ -55,7 +63,7 @@ class TwitterPoll(threading.Thread):
 
                 if tw_timestamp < self.timestamp:
                     if tl == timeline[0]:
-                        print("No new tweets (%s)" % time())
+                        self._invoke_event("no_tweets")
 
                     break
                 else:
@@ -64,11 +72,56 @@ class TwitterPoll(threading.Thread):
                 user = tl["user"]["screen_name"]
                 tweet = tl["text"].replace(u'\u2019', "'").replace(u'\u201c', "\"").replace(u'\u2018', "'").replace(u'\u201d', "\"")
 
-                print("TWEET by %s(%s): %s\n\n" % (user, tl["created_at"], tweet))
+                self._invoke_event("new_tweet", user, tweet, tl)
 
                 #TODO: Add bot hooks/callbacks
 
             sleep(self.polling_seconds)
+
+    def register_event(self, argument):
+        """A decorator that registers an event to listen to.
+
+        You can find more info about the events on the :ref:`documentation below <discord-api-events>`.
+
+        Example: ::
+
+            @tp.event
+            def new_tweet():
+                print('Ready!')
+
+            @tp.event("new_tweet")
+            def test():
+                print('Ready!')
+        """
+
+        if type(argument) == str:
+            def real_decorator(function):
+
+                event_name = argument
+
+                if event_name not in self.events:
+                    raise Exception('The function name "{}" is not a valid event name'.format(event_name))
+
+                self.events[event_name] = function
+
+                return function
+
+            return real_decorator
+        else:
+            function = argument
+
+            if function.__name__ not in self.events:
+                raise Exception('The function name "{}" is not a valid event name'.format(function.__name__))
+
+            self.events[function.__name__] = function
+
+            return function
+
+    def _invoke_event(self, event_name, *args, **kwargs):
+        try:
+            self.events[event_name](*args, **kwargs)
+        except Exception as e:
+            print("Error during %s, %s" % (event_name, e))
 
 if __name__ == "__main__":
     try:
@@ -88,5 +141,13 @@ if __name__ == "__main__":
 
     tp = TwitterPoll(access_token_key, access_token_secret, consumer_key, consumer_secret)
     tp.start()
+
+    @tp.register_event("new_tweet")
+    def test(user, tweet, tweetdata):
+        print("TWEET2 by %s(%s): %s\n\n" % (user, tweetdata["created_at"], tweet))
+
+    @tp.register_event
+    def no_tweets():
+        print("saddness, nothing new :/")
 
     tp.join(timeout=60 * 15)
